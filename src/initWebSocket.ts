8c46ec4c6f08c45783038b215c2535c9e8dc64ab
Zomsik <zomsix@gmail.com>
2023-05-07 22:32:03 +0200

    Started creating middleware

--- src/initWebSocket.ts
+++ src/initWebSocket.ts
@@ -5,9 +5,23 @@ import { Server, Socket } from "socket.io";
 export default function initWebSocket(server: HttpServer) {
     const io = new Server(server, {});
 
-    io.on('connection', (socket: Socket) => {
+    io.use((socket, next) => {
+        const headers = socket.handshake.headers;
+
+        if(!headers.server) {
+            next(new Error('Disconnected - no server id!'))
+        }
+
+        next();
+      })
+    .on('connection', (socket: Socket) => {
         const request = socket.handshake;
-  
+
+        socket.on('message', function(message) {
+            io.emit('message', "return");
+            console.log("Received: "+message)
+        });
+
         socket.on('disconnect', () => {
             console.log('Client disconnected');
         });
