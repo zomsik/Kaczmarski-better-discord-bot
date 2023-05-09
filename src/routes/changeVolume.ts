07954c2a7c2e7318a86d7e8d1589b07fb3d0abc5
Zomsik <zomsix@gmail.com>
2023-05-09 20:38:22 +0200

    Refactoring of api changevolume route

--- src/routes/changeVolume.ts
+++ src/routes/changeVolume.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from 'express';
 import readServerVariables from '../functions/readServerVariables';
 import sendMessage from '../functions/sendMessage';
 import KaczmarskiClient from '../KaczmarskiClient';
+import changeVolume from '../functions/changeVolume';
 
 const router = Router();
 
@@ -18,7 +19,6 @@ router.get('/', async (req: Request, res: Response) => {
         if (req.query.apiPassword == apiPassword || req.query.apipassword == apiPassword || !apiPassword) {
             let queue = useQueue(guildID);
 
-    
             if (queue) {
                 const volume: number = Number(req.query.volume as string);
                 const operation = req.query.operation as string;
@@ -30,61 +30,22 @@ router.get('/', async (req: Request, res: Response) => {
                     return;
                 }
 
-        
-                if (volume > 100 || volume < 0)
-                {
+                if (volume > 100 || volume < 0) {
                     res.status(200).send({ message: "Volume level must be a number between 0-100" });
                     return;
                 }
-                else
-                {
-
-                    switch (operation)
-                    {
-
-                        case "add": {
-                            if (volume+previousVolume >= 100) {
-                                queue.node.setVolume(100);
-                            } else {
-                                queue.node.setVolume(previousVolume+volume);
-                            }
-
-                            res.status(200).send({ message: `Volume level set to ${queue.node.volume}` });
-                            if (channelID)
-                                sendMessage(guildID, channelID, `Volume changed with API to ${queue.node.volume}!`, client);
-                            break;
-                        }                     
-                        case "sub": {
-                            if (previousVolume-volume <= 0) {
-                                queue.node.setVolume(0);
-                            } else {
-                                queue.node.setVolume(previousVolume-volume);
-                            }
-
-                            res.status(200).send({ message: `Volume level set to ${queue.node.volume}` });
-                            if (channelID)
-                                sendMessage(guildID, channelID, `Volume changed with API to ${queue.node.volume}!`, client);
-                            break;
-                        } 
-                        case "set": {
-                            queue.node.setVolume(volume);
-                            res.status(200).send({ message: `Volume level set to ${queue.node.volume}` });
-                            if (channelID)
-                                sendMessage(guildID, channelID, `Volume changed with API to ${queue.node.volume}!`, client);
-                            break;
-                        }
-                        default: {
-                            res.status(200).send({ message: `Undefined volume change operation` });
-                            break;
-                        }
-                    }
-
+                else {
+                  if (changeVolume(queue, volume, operation, previousVolume)) {
+                    res.status(200).send({ message: `Volume level set to ${queue.node.volume}` });
+                    if (channelID)
+                        sendMessage(guildID, channelID, `Volume changed with API to ${queue.node.volume}!`, client);
+                  } else {
+                    res.status(200).send({ message: `Undefined volume change operation` });
+                  }
                 }
-            }
-            else {
+            } else {
                 res.status(200).send({ message: "Could not set volume" });
             }
-
         } else {
           res.status(200).send({ message: "No access" });
         }
